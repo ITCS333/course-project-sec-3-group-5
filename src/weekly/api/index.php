d0bcfcea21ea2b0692a3d2fdb274179b2627fa06
Khadijax9 <202206338@stu.uob.edu.bh>
2026-05-21 15:45:46 +0300

    1

--- src/weekly/api/index.php
+++ src/weekly/api/index.php
@@ -538,16 +538,14 @@ function deleteComment(PDO $db, $commentId): void
 
     // TODO: DELETE FROM comments_week WHERE id = ?
     $stmt = $db->prepare("DELETE FROM comments_week WHERE id = ?");
-   
+   $stmt->execute([$commentId]);
 
     // TODO: If rowCount() > 0, sendResponse HTTP 200.
     // Otherwise sendResponse HTTP 500.
-    $stmt->execute([$commentId]);
-
- if ($stmt->rowCount() > 0) {
-        sendResponse(['success' => true, 'message' => 'Week deleted successfully'], 200);
-    } else {
-        sendResponse(['success' => false, 'message' => 'Failed to delete week'], 500);}
+  sendResponse([
+        'success' => true,
+        'message' => 'Comment deleted successfully'
+    ], 200);
 
 }
 
